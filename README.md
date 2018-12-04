07d4f4f38ae0bd1b5f6613a277338a0f5b2b9921
KieranVieira <kieranvieira@live.com>
2018-12-03 20:58:51 -0500

    Added services section

--- README.md
+++ README.md
@@ -5,12 +5,13 @@ This is a personal portfolio website mockup that I'm creating for myself as a gr
 
 The tech stack for this project is always changing and evolving as I build out my personal portfolio. I will list all the languages and frameworks used in this project and edit the README.md whenever I add/remove certain languages.
 
-## The tech is as follows:
+### The tech is as follows:
 
 * HTML
 * CSS
 * JavaScript
 * LESS.js http://lesscss.org/
-* JQuery 2.2.4 https://code.jquery.com/ https://code.jquery.com/jquery-2.2.4.min.js
+* JQuery 2.2.4 https://code.jquery.com/ https://code.jquery.com/jquery-2.2.4.min.js (being used temporarily for the typing animation)
 
-### More tech will be added and removed as I see fit.
+#### More tech will be added and removed as I see fit.
+
--- js/type.js
+++ js/type.js
@@ -9,14 +9,14 @@ $(document).ready(function() {
 });
 
 function typingEffect() {
-    caption = '<p>Full stack web developer/Graphic Designer</p> <br/> <p>Email: Kieranvieira@live.com</p> <br/> <p>Role: Student</p> <br/> <p>Bio: I am a passionate Graphic Designer currently working on improving my front end development skills using languages such as HTML, CSS, JavaScript, and other frameworks. I have been doing freelance web development jobs using Wordpress CMS, HTML, and CSS for 2 years and Graphic Design for almost 6 years.</p> <br/> <p class="continue"><a href="#portfolio">Click here or press space to continue <span class="blink">_</span></a></p>';
+    caption = '<p>Full Stack Web Developer/Graphic Designer</p> <br/> <p>Email: Kieranvieira@live.com</p> <br/> <p>Role: Student</p> <br/> <p>Bio: I am a passionate Graphic Designer currently working on improving my front end development skills using languages such as HTML, CSS, JavaScript, and other frameworks. I have been doing freelance web development jobs using Wordpress CMS, HTML, and CSS for 2 years and Graphic Design for almost 6 years.</p> <br/> <p class="continue"><a href="#portfolio">Click here or press space to continue <span class="blink">_</span></a></p>';
     type();
 }
 
 function type() {
     captionEl.html(caption.substr(0, captionLength++));
     if(captionLength < caption.length+1) {
-        setTimeout('type()', 50);
+        setTimeout('type()', 40);
     } else {
         captionLength = 0;
         caption = '';
