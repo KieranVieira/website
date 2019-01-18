3dae7b23f7707b4beb404618c9d84495c18980c1
KieranVieira <kieranvieira@live.com>
2019-01-17 23:25:23 -0500

    Changed spelling on BSoD and added text-align: left for older browser support

--- js/type.js
+++ js/type.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 });
 
 function typingEffect() {
-    caption = '<p>Full Stack Software Engineer/Graphic Designer</p> <br/> <p>Email: Kieranvieira@live.com</p> <br/> <p>Role: Student</p> <br/> <p>Bio: I am a passionate Software Engineer currently focussed on front end development skills using languages such as HTML, CSS, JavaScript, and other frameworks. I have been doing freelance web development jobs using Wordpress CMS, HTML, CSS, and JS for 2 years plus Graphic Design for almost 6 years. Email me for more info.</p> <br/> <p class="continue"><a href="#services">Click here to continue <span class="blink">_</span></a></p>';
+    caption = '<p>Full Stack Software Engineer/Graphic Designer</p> <br/> <p>Email: Kieranvieira@live.com</p> <br/> <p>Role: Student</p> <br/> <p>Bio: I am a passionate Software Engineer currently focused on front end development skills using languages such as HTML, CSS, JavaScript, and other frameworks. I have been doing freelance web development jobs using Wordpress CMS, HTML, CSS, and JS for 2 years plus Graphic Design for almost 6 years. Email me for more info.</p> <br/> <p class="continue"><a href="#services">Click here to continue <span class="blink">_</span></a></p>';
     type();
 }
 
