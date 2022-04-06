163d653aab8f86f95a9e78033559548a8fde6b73
Kieran Vieira <kieranvieira@live.com>
2022-04-06 02:36:23 -0400

    Added initial icon and image assets

---
+++ src/assets/index.ts
@@ -0,0 +1,37 @@
+import arrow from '../assets/icons/arrow.svg';
+import docker from '../assets/icons/docker.svg';
+import git from '../assets/icons/git.svg';
+import graphql from '../assets/icons/graphql.svg';
+import k8s from '../assets/icons/k8s.svg';
+import nodejs from '../assets/icons/nodejs.svg';
+import psql from '../assets/icons/psql.svg';
+import react from '../assets/icons/react.svg';
+import styledComponents from '../assets/icons/styled-components.svg';
+import tailwind from '../assets/icons/tailwind.svg';
+import typescript from '../assets/icons/typescript.svg';
+import unity from '../assets/icons/unity.svg';
+
+import bigflop from '../assets/images/bigflop.png';
+import displayPicture from '../assets/images/display_picture.jpg';
+
+const icons = {
+  arrow,
+  docker,
+  git,
+  graphql,
+  k8s,
+  nodejs,
+  psql,
+  react,
+  styledComponents,
+  tailwind,
+  typescript,
+  unity
+}
+
+const images = {
+  bigflop,
+  displayPicture
+}
+
+export { icons, images };
