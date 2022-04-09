a013e392b2ee270d7ed2250962e58a99c105d8a8
Kieran Vieira <kieranvieira@live.com>
2022-04-09 00:16:44 -0400

    Updated article card component

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,4 +1,6 @@
-import { Icons } from '../assets';
+import Image from 'next/image';
+
+import { icons } from '../assets';
 
 interface Props {
   title: string;
@@ -13,7 +15,7 @@ export const ArticleCard: React.FC<Props> = ({ title, label }) => {
           <h3>{title}</h3>
           <h6 className='mt-1'>Article</h6>
         </div>
-        <Icons.Arrow />
+        <Image src={icons.arrow} draggable={false} />
       </div>
       <div className='absolute flex items-center justify-center right-0 -z-10 -top-6 pt-1 pb-5 px-3 bg-paleBlue rounded-t-md'>
         <h6>{label}</h6>
